deb33de66f1f40c6af7181e0db08ed6120d21b9a
Yathaartha <maharjanyathaartha017@gmail.com>
2021-03-07 20:50:29 +0545

    made some changes to the messages displayed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,18 +1,19 @@
 class randomZodiac {
   constructor() {
     this.zodiac = [
-      "Aries",
-      "Taurus",
-      "Gemini",
-      "Cancer",
-      "Leo",
-      "Virgo",
-      "Libra",
-      "Scorpio",
-      "Sagittarius",
-      "Capricorn",
-      "Aquarius",
-      "Pisces",
+      "Happy Birthday!! May all your wishes come true.",
+      "Let's meet at the park at 8pm tomorrow",
+      "How was your day today?",
+      "Do you like icecream? If yes which flavor?",
+      "Let's go on a long drive looking for mother nature",
+      "The fear of Covid has really gone down these past few weeks.",
+      "Come to the principal's office immediately.",
+      "You have a meeting at 5pm. Don't miss it.",
+      "I hope your day was cheerful today.",
+      "Congratulations on getting a new bike",
+      "Wow, you look really pretty today.",
+      "Summer is starting soon, better get ready for the mosquitoes.",
+      "Time, Reality, Space, Mind, Power and Soul, acquire all these stones and you will become the most powerfl being.",
     ];
   }
   getRandomZodiac() {
@@ -26,25 +27,25 @@ class randomZodiac {
   }
 }
 
-class randomNumber {
-  constructor() {
-    this.rndNum = Math.floor(Math.random() * 100);
-    console.log(this.rndNum);
-  }
+// class randomNumber {
+//   constructor() {
+//     this.rndNum = Math.floor(Math.random() * 100);
+//     console.log(this.rndNum);
+//   }
 
-  getLuckyNum() {
-    return this.rndNum;
-  }
+//   getLuckyNum() {
+//     return this.rndNum;
+//   }
 
-  renderLuckyNum() {
-    const luckynum = document.getElementById("luckynum");
-    luckynum.textContent = this.getLuckyNum();
-    console.log(this.rndNum);
-  }
-}
+//   renderLuckyNum() {
+//     const luckynum = document.getElementById("luckynum");
+//     luckynum.textContent = this.getLuckyNum();
+//     console.log(this.rndNum);
+//   }
+// }
 
-const rng = new randomNumber();
-rng.renderLuckyNum();
+// const rng = new randomNumber();
+// rng.renderLuckyNum();
 
 const rnz = new randomZodiac();
 rnz.renderZodiac();
